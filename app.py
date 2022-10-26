58debce813db6eff539dde9c5dc3e66f35429bae
MoJaeYoung <jym2013@gmail.com>
2022-10-26 17:08:14 +0900

    완성작

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
-
+from pymongo import MongoClient
+import certifi
+ca = certifi.where()
+client = MongoClient('mongodb+srv://mo:jae@cluster0.joa3ijk.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
 @app.route('/')
 def home():
    return render_template('index.html')
 
 @app.route("/homework", methods=["POST"])
 def homework_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
+    nick_receive = request.form['nick_give']
+    reple_receive = request.form['reple_give']
+
+    doc = {
+        'nick': nick_receive,
+        'reple': reple_receive
+    }
+    db.kkk.insert_one(doc)
+
     return jsonify({'msg':'POST 연결 완료!'})
 
 @app.route("/homework", methods=["GET"])
 def homework_get():
-    return jsonify({'msg':'GET 연결 완료!'})
+    reple_list = list(db.kkk.find({}, {'_id': False}))
+    print(reple_list)
+    return jsonify({'reple':reple_list})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5001, debug=True)
